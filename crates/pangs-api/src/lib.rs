f9c67e9893378a511d958d12c8c0a9f1573f9776
Ben Karel <bkarel@aarno-labs.com>
2026-07-22 12:57:01 -0400

    Ran `cargo fmt`

--- crates/pangs-api/src/lib.rs
+++ crates/pangs-api/src/lib.rs
@@ -1267,7 +1267,8 @@ impl Analysis {
                 let pag_started = Instant::now();
                 let indirect_vararg_keys = indirect_callsites
                     .iter()
-                    .filter(|&(cs, _, sig)| sig.vararg).map(|(cs, _, sig)| callsites[cs.0 as usize].key.clone())
+                    .filter(|&(cs, _, sig)| sig.vararg)
+                    .map(|(cs, _, sig)| callsites[cs.0 as usize].key.clone())
                     .collect::<BTreeSet<_>>();
                 let mut pag = Pag::from_pir(
                     module,
@@ -2523,9 +2524,7 @@ fn collect_targeted_stationarity_writers(
             .into_iter()
             .map(|(global, writers)| (global, StationarityWriters::from(writers)))
             .collect(),
-        target_unknown_writers
-            .into_iter()
-            .collect(),
+        target_unknown_writers.into_iter().collect(),
     )
 }
 
